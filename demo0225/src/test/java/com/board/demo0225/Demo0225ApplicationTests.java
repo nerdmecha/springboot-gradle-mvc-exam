f05c9902a5c41f12c945a217745b79075b06073e
Jong Hun Kim <corner.jhkim@gmail.com>
2022-05-25 13:55:01 +0900

    Added Another Project

---
+++ demo0225/src/test/java/com/board/demo0225/Demo0225ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.demo0225;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0225ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
