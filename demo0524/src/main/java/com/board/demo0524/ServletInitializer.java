1ecee1c313ff604b1d4c188c3d410b4c50826cae
Jong Hun Kim <corner.jhkim@gmail.com>
2022-05-25 13:32:25 +0900

    Added src Folder

---
+++ demo0524/src/main/java/com/board/demo0524/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.board.demo0524;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Demo0524Application.class);
+	}
+
+}
