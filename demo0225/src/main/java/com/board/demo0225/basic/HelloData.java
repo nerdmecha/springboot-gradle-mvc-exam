047e65e0fd142ad984b8e1b3f72d7ee81a320bbc
Jong Hun Kim <corner.jhkim@gmail.com>
2022-05-26 11:14:22 +0900

    Add files via upload

---
+++ demo0225/src/main/java/com/board/demo0225/basic/HelloData.java
@@ -0,0 +1,25 @@
+package com.board.demo0225.basic;
+
+import lombok.Data;
+
+@Data
+public class HelloData {
+	private String username;
+	private int age;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
